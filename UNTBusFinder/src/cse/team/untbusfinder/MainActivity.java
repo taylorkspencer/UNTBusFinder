980aaeff4daff48e11c505ea762f722b302d0584
taylorkspencer@gmail.com <taylorkspencer@Taylorlaptop>
2014-02-20 14:48:25 -0600

    Claiming as mine!

--- UNTBusFinder/src/cse/team/untbusfinder/MainActivity.java
+++ UNTBusFinder/src/cse/team/untbusfinder/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends Activity
 		
 	}
 	
-	//TODO: Take the user's coordinates and displays them on a map
+	//TODO: Take the user's coordinates and displays them on a map (Taylor's)
 	// Called when the user clicks the Map Coordinates button
 	public void map_coordinates(View view)
 	{
