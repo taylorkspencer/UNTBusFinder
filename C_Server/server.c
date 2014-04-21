d1ee7657c97df850a16890d2716a89245ef41e64
ManuelGGottardi <ManuelGGottardi@gmail.com>
2014-04-21 12:27:07 -0500

    C Server implemented with the ability to receive GET and POST requests. The server currently does not even bother to look at the request itself, if simply stores the location string as it is sent for a POST request and then returns that string (with some http headers of course) for a GET request. If we get this working, it would be a simple thing to scale this up to allow for multiple routes.

--- C_Server/server.c
+++ C_Server/server.c
@@ -1,58 +1,32 @@
-/***********************************************************
-Filename: server.c
-
-Description: This is a simple implementation of an http
-server process. It is intended to receive a message with
-a filename as the content, search the working directory for 
-the file, and then return the file as an http response. The
-file requested is assumed to be a plain text file. Please note
-that the filename must be preceded by a '/'. Please also note that
-if this program is immediately restarted, the port may not be useable.
-So if you intend to restart this program, please wait at least
-one minute before doing so.
-
-Author: I believe the original code was written by
-Evan Baatarjav, but I am not entirely certain. It was
-provided by him on the CSCE 3530 course website but had no author
-specified.
-
-Modified by: Manuel Gottardi
-
-Last modified: April 1, 2014
-************************************************************/
-
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdio.h>
 #include <netinet/in.h>
+#include <netdb.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define SIZE 1000
-#define BUFFERSIZE 1000
-//#define SERVER_IP "192.168.1.5" // home
-#define SERVER_IP "10.124.4.235" // UNT
+#define BUFFERSIZE 500
+#define SERVER_IP "192.168.1.19" // Raspberry Pi
 #define SERVER_PORT 31588
 
-int main ()
-{
-	int server_sockfd, client_sockfd;
-	int server_len, client_len;
-	int fdread, szread, szwrite, i, exit = 0;
-	char buff[SIZE], file_requested[100];
-	char *pstring;
+int main (){
+	int server_sockfd, client_sockfd, bytesReceived, server_len;
+	int client_len, i, j;
+	int word_size;
+	char *pstring, message[BUFFERSIZE], location[BUFFERSIZE];
+	char temp[BUFFERSIZE];
+	char request[300], message_body[BUFFERSIZE];
 	struct sockaddr_in server_address;
-	struct sockaddr_in client_address;
-	int bytesReceived = 0;
-	char message[BUFFERSIZE];
+	struct sockaddr_in client_address;	
 
 	// Create an unnamed socket
 	server_sockfd = socket (AF_INET, SOCK_STREAM, 0);
 	if (server_sockfd==-1){
-		perror("Create socket");
+		perror("Create proxy socket"); // Debug
 		return(0);
 	}
 
@@ -61,72 +35,91 @@ int main ()
 	server_address.sin_addr.s_addr = inet_addr (SERVER_IP);
 	server_address.sin_port = htons(SERVER_PORT);
 	server_len = sizeof (server_address);
-	bind (server_sockfd, (struct sockaddr *) &server_address, server_len);
+	if(bind(server_sockfd, (struct sockaddr *) &server_address, server_len) == -1){
+		printf("\nSystem has not released port. Try again in a minute.\n\n"); // Debug
+		close(server_sockfd);
+		return 0;
+	}
 
 	// Create a connection queue and wait for clients to arrive
-	listen (server_sockfd, 10);
+	listen (server_sockfd, 1000);
 
+	memset(location, '\0', BUFFERSIZE);
+
+	// Main loop for the program
 	while(1){
+		// Connect to client
 		// Make a connection when a request is received
 		client_len = sizeof (client_address);
 		client_sockfd = accept (server_sockfd, (struct sockaddr *) &client_address, &client_len);
 		if (client_sockfd<0){
-			perror("Accept connection");
+			perror("Accept connection"); // Debug
+			close (client_sockfd);
+			continue;
+		}
+		printf ("Server connected to client.\n\n"); // Debug
+
+		// Receive request from the client
+		memset(message, '\0', BUFFERSIZE);
+		bytesReceived = recv(client_sockfd, message, BUFFERSIZE, 0);
+
+		// If empty request, ignore
+		if(bytesReceived < 1){
+			printf("Empty HTTP request. Ignoring.\n\n"); // Debug
 			close(client_sockfd);
 			continue;
 		}
-		printf ("server waiting\n");
+		//printf("Received message from client.\n\n"); // Short debug
+		printf ("Received message from %s\n\n%s\n\n", inet_ntoa(client_address.sin_addr), message); // Long debug
 
-		// Receive request from the client (only able to receive GET requests)
-		bytesReceived = recv(client_sockfd, message, BUFFERSIZE, 0);
-		message[bytesReceived] = '\0'; // properly terminate string
-		printf ("received from %s\n%s", inet_ntoa (client_address.sin_addr), message);
-
-		// Parse message for file requested
-		pstring = strstr(message, "GET");
-		if(pstring == NULL){
-			printf("ERROR! User made request other than GET\n");
-			write (client_sockfd, "HTTP/1.1 501 Not Implemented\n", 15);
-			close (client_sockfd);
-			continue;
-		}
-		pstring += 4;
-
-		// Copy filename to file_requested	
+		// Get request
 		i = 0;
-		file_requested[0] = '.';
-		while(pstring[i] != ' ' && pstring[i] != '\n' && pstring[i] != EOF){
-			file_requested[i+1] = pstring[i];
-			i++;
-		}
-		file_requested[i+1] = '\0';
-		//printf("File requested is: %s\n", file_requested);
-
-		// Attempt to open file in READ ONLY mode
-		fdread = open(file_requested, O_RDONLY); // Open the read file in read only mode
-		if(fdread == -1) // If either file could not be opened/created, exit program with error message
-		{
-			printf("ERROR! Could not open the requested file.\n");
-			write (client_sockfd, "HTTP/1.1 404 Not Found\n\n", 24);
-			close (client_sockfd);
+		if((pstring = strstr(message, "GET")) != NULL){
+			printf("Received get request. Sending the following response:\n\n"); // Debug
+			write(client_sockfd, "HTTP/1.1 200 OK\r\n", 17);
+			printf("HTTP/1.1 200 OK\r\n");
+			write(client_sockfd, "Connection: close\r\n", 19);
+			printf("Connection: close\r\n");
+			write(client_sockfd, "Content-Type: application/x-www-form-urlencoded\r\n\r\n", 51); // Modify this to be accurate
+			printf("Content-Type: application/x-www-form-urlencoded\r\n\r\n");
+			write(client_sockfd, location, strlen(location));
+			printf("%s\n\n", location);
+			close(client_sockfd);
 			continue;
 		}
 
-		// If file was successfully opened, stream it to the client after adding appropriate headers
-		write(client_sockfd, "HTTP/1.1 200 OK\nContent-Type: text/plain; charset=us-ascii\nConnection: close\n\n", 78); 
+		else{
+			if((pstring = strstr(message, "POST")) == NULL){
+				printf("Received unknown request type. Sending error response to client and discarding.\n\n"); // Debug
+				write(client_sockfd, "HTTP/1.1 400 Bad Request\r\n", 26);
+				write(client_sockfd, "Connection: close\r\n\r\n", 21);
+				close(client_sockfd);
+				continue;
+			}
 
-		// Stream file
-		while(exit != 1) // While exit flag has not been set
-		{
-			szread = read(fdread, buff, SIZE); // Read the first "chunk" of size SIZE from the file and store in buffer buff
-			szwrite = write(client_sockfd, buff, szread); // Write to the client what was read
-			if(szwrite < SIZE) // If the end of the file was reached, exit the loop
-				exit = 1;
+			else{
+				printf("Received a POST request. Sending the following response:\n\n"); // Debug
+				pstring = strstr(message, "\n\r\n");
+				if(pstring == NULL){
+					printf("No body. Discarding\n\n"); // Debug
+					write(client_sockfd, "HTTP/1.1 400 Bad Request\r\n", 26);
+					write(client_sockfd, "Connection: close\r\n\r\n\r\n\r\n", 25);
+					close(client_sockfd);
+					continue;
+				}
+
+				pstring += 3;
+				memset(location, '\0', BUFFERSIZE);
+				strcpy(location, pstring);
+				write(client_sockfd, "HTTP/1.1 201 Created\r\n", 22);
+				printf("HTTP/1.1 201 Created\r\n");
+				write(client_sockfd, "Connection: close\r\n\r\n", 21);
+				printf("Connection: close\r\n\r\n");
+				close(client_sockfd);
+				printf("Saved the following location data: %s\n\n", location); //Debug
+				continue;
+			}
 		}
-
-		// Close all streams
-		close(fdread);
-		close(client_sockfd);
 	}
 	close (server_sockfd);
 	return 0;
