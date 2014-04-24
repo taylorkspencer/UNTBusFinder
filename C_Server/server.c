d0fb467f1252e2d11e16dff11e0e977fe2287eff
ManuelGGottardi <ManuelGGottardi@gmail.com>
2014-04-23 21:49:33 -0500

    A couple minor server adjustments. Upon restarting the server, the app used to crash due to the server sending blank coordinates. Default coordinates have now been written in so that this does not happen. Also, the output is a bit more versatile now, in that it can relatively easily be switched between terminal output and having the output written to a file. Lastly, the buffer size for waiting connection requests has been reduced greatly, so at to avoid a potential buildup and the resulting old info that would be introduced by them.

--- C_Server/server.c
+++ C_Server/server.c
@@ -18,6 +18,7 @@ int main (){
 	int client_len, i, j;
 	int word_size;
 	char *pstring, message[BUFFERSIZE], location[100];
+	strcpy(location, "{ \"lat\": 33.168633, \"long\": -97.068325 }\0");
 	char temp[BUFFERSIZE];
 	char request[300], message_body[BUFFERSIZE];
 	struct sockaddr_in server_address;
@@ -27,7 +28,10 @@ int main (){
 	// Create an unnamed socket
 	server_sockfd = socket (AF_INET, SOCK_STREAM, 0);
 	if (server_sockfd==-1){
-		perror("Create proxy socket"); // Debug
+		//output = fopen("output.txt", "a");
+		//fprintf(output, "\nCreate proxy socket\n") // File
+		//fclose(output);
+		perror("Create proxy socket"); // Terminal
 		return(0);
 	}
 
@@ -37,15 +41,16 @@ int main (){
 	server_address.sin_port = htons(SERVER_PORT);
 	server_len = sizeof (server_address);
 	if(bind(server_sockfd, (struct sockaddr *) &server_address, server_len) == -1){
-		printf("\nSystem has not released port. Try again in a minute.\n\n"); // Debug
+		//output = fopen("output.txt", "a");
+		//fprintf(output, "\nSystem has not released port. Try again in a minute.\n\n"); // File
+		//fclose(output);
+		printf("\nSystem has not released port. Try again in a minute.\n\n"); // Terminal
 		close(server_sockfd);
 		return 0;
 	}
 
 	// Create a connection queue and wait for clients to arrive
-	listen (server_sockfd, 1000);
-
-	memset(location, '\0', 100);
+	listen (server_sockfd, 4);
 
 	// Main loop for the program
 	while(1){
@@ -54,11 +59,17 @@ int main (){
 		client_len = sizeof (client_address);
 		client_sockfd = accept (server_sockfd, (struct sockaddr *) &client_address, &client_len);
 		if (client_sockfd<0){
-			perror("Accept connection"); // Debug
+			//output = fopen("output.txt", "a");
+			//fprintf(output, "\nAccept connection\n"); // File
+			//fclose(output);
+			perror("Accept connection"); // Terminal
 			close (client_sockfd);
 			continue;
 		}
-		//printf ("Server connected to client.\n\n"); // Debug
+		//output = fopen("output.txt", "a");
+		//fprintf(output, "Serverconnected to client.\n\n"); // File
+		//fclose(output);
+		printf ("Server connected to client.\n\n"); // Terminal
 
 		// Receive request from the client
 		memset(message, '\0', BUFFERSIZE);
@@ -66,53 +77,66 @@ int main (){
 
 		// If empty request, ignore
 		if(bytesReceived < 1){
-			printf("Empty HTTP request. Ignoring.\n\n"); // Debug
+			//output = fopen("output.txt", "a");
+			//fprintf(output, "Empty HTTP request. Ignoring.\n\n"); // File
+			//fclose(output);
+			printf("Empty HTTP request. Ignoring.\n\n"); // Terminal
 			close(client_sockfd);
 			continue;
 		}
-		//printf("Received message from client.\n\n"); // Short debug
-		//printf ("Received message from %s\n\n%s\n\n", inet_ntoa(client_address.sin_addr), message); // Long debug
+		//output = fopen("output.txt", "a");
+		//fprintf(outlook, "Received message from %s\n\n%s\n\n", inet_ntoa(client_address.sin_addr), message); File
+		//fclose(output);
+		printf ("Received message from %s\n\n%s\n\n", inet_ntoa(client_address.sin_addr), message); // Terminal
 
 		// Get request
 		i = 0;
 		if((pstring = strstr(message, "GET")) != NULL){
-			output = fopen("output.txt", "a");
-			fprintf(output, "Received get request. Sending the following response:\n\n"); // Debug
+			//output = fopen("output.txt", "a");
+			//fprintf(output, "Received GET request. Sending the following response:\n\n"); // File
+			printf("Received GET request. Sending the following response:\n\n"); // Terminal
 			write(client_sockfd, "HTTP/1.1 200 OK\r\n", 17);
-			fprintf(output, "HTTP/1.1 200 OK\r\n");
+			//fprintf(output, "HTTP/1.1 200 OK\r\n"); // File
+			printf("HTTP/1.1 200 OK\r\n"); // Terminal
 			write(client_sockfd, "Connection: close\r\n", 19);
-			fprintf(output, "Connection: close\r\n");
-			write(client_sockfd, "Content-Type: application/x-www-form-urlencoded\r\n\r\n", 51); // Modify this to be accurate
-			fprintf(output, "Content-Type: application/json\r\n\r\n");
+			//fprintf(output, "Connection: close\r\n"); // File
+			printf("Connection: close\r\n"); // Terminal
+			write(client_sockfd, "Content-Type: application/json\r\n\r\n", 34); // Modify this to be accurate
+			//fprintf(output, "Content-Type: application/json\r\n\r\n"); // File
+			printf("Content-Type: application/json\r\n\r\n"); //Terminal
 			write(client_sockfd, location, strlen(location));
 			write(client_sockfd, "\r\n\r\n", 4);
-			fprintf(output, "%s\n\n", location);
-			fclose(output);
+			//fprintf(output, "%s\n\n", location); // File
+			printf("%s\n\n", location); // Terminal
+			//fclose(output);
 			close(client_sockfd);
 			continue;
 		}
 
 		else{
 			if((pstring = strstr(message, "POST")) == NULL){
-				output = fopen("output.txt", "a");
-				fprintf(output, "Received unknown request type. Sending error response to client and discarding.\n\n"); // Debug
+				//output = fopen("output.txt", "a");
+				//fprintf(output, "Received unknown request type. Sending error response to client and discarding.\n\n"); // File
+				printf("Received unknown request type. Sending error response to client and discarding.\n\n"); //Terminal
 				write(client_sockfd, "HTTP/1.1 400 Bad Request\r\n", 26);
 				write(client_sockfd, "Connection: close\r\n\r\n", 21);
-				fclose(output);
+				//fclose(output);
 				close(client_sockfd);
 				continue;
 			}
 
 			else{
-				output = fopen("output.txt", "a");
-				fprintf(output, "Received a POST request. Sending the following response:\n\n"); // Debug
+				//output = fopen("output.txt", "a");
+				//fprintf(output, "Received a POST request. Sending the following response:\n\n"); // File
+				printf("Received a POST request. Sending the following response:\n\n"); // Terminal
 				pstring = strstr(message, "\n\r\n");
 				if(pstring == NULL){
-					fprintf(output, "No body. Discarding\n\n"); // Debug
+					//fprintf(output, "No body. Discarding\n\n"); // String
+					printf("No body. Discarding\n\n"); // Terminal
 					write(client_sockfd, "HTTP/1.1 400 Bad Request\r\n", 26);
 					write(client_sockfd, "Connection: close\r\n\r\n\r\n\r\n", 25);
 					close(client_sockfd);
-					fclose(output);
+					//fclose(output);
 					continue;
 				}
 
@@ -138,15 +162,19 @@ int main (){
 
 				// Send response
 				write(client_sockfd, "HTTP/1.1 201 Created\r\n", 22);
-				fprintf(output, "HTTP/1.1 201 Created\r\n"); // Debug
+				//fprintf(output, "HTTP/1.1 201 Created\r\n"); // File
+				printf("HTTP/1.1 201 Created\r\n"); // Terminal
 				write(client_sockfd, "Content-Type: text\r\n", 20);
-				fprintf(output, "Content-Type: text\r\n");
+				//fprintf(output, "Content-Type: text\r\n"); // File
+				printf("Content-Type: text\r\n"); // Terminal
 				write(client_sockfd, "Connection: close\r\n\r\n", 21);
-				fprintf(output, "Connection: close\r\n\r\n"); // Debug
+				//fprintf(output, "Connection: close\r\n\r\n"); // File
+				printf("Connection: close\r\n\r\n"); // Terminal
 				write(client_sockfd, location, strlen(location));
-				fprintf(output, "%s\n\n", location); // Debug
+				//fprintf(output, "%s\n\n", location); // File
+				printf("%s\n\n"); // Terminal
 				write(client_sockfd, "\r\n\r\n\0", 5);
-				fclose(output);
+				//fclose(output);
 				close(client_sockfd);
 				continue;
 			}
